d8c6f89f00921cc9d8fd3cd6cb3a166e43abdc51
Francesco Zaniol <fzaniol@gmail.com>
2024-03-12 15:20:27 +0100

    misc layout

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="h-[60px] relative">
+    <div class="fixed top-0 left-0 right-0 z-50 h-[60px] bg-slate-200">
+      <nav class="container mx-auto flex items-center justify-between p-4 z-50">
+        <div class="z-50">
+          <nuxt-link to="/" class="text-xl font-bold border-none z-50 ">
+            WeBookAndTravel
+          </nuxt-link>
+        </div>
+        <div class="hidden lg:flex items-center space-x-8">
+          <nuxt-link to="/" class="border-none">Book</nuxt-link>
+          <nuxt-link to="/travel" class="border-none">Travel</nuxt-link>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="h-full grid grid-rows-[60px_1fr] items-start">
+    <Header />
     <div class="p-5 justify-self-center container">
       <slot />
     </div>
