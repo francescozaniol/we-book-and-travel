9f6af9285f8a9b125b7bb9ca055f9dee9873c46d
Francesco Zaniol <fzaniol@gmail.com>
2024-03-15 17:16:12 +0100

    fixed eslint minor issue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-full grid grid-rows-[60px_1fr] grid-cols-[100%] items-start">
-    <Header />
+    <TheHeader />
     <div class="justify-self-center w-full">
       <slot />
     </div>
